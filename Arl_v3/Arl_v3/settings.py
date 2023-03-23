e30f4c3be6d2d9592bedd3f741277b3681af123d
Satyam Pandey <satyampandey.dev23@gmail.com>
2023-03-24 01:00:28 +0530

    adding static file and templates

--- Arl_v3/Arl_v3/settings.py
+++ Arl_v3/Arl_v3/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Arl_v3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT="/Arl_v3/static"
+STATICFILES_DIRS = [
+    BASE_DIR,'static'
+    ]
 
 
 # Default primary key field type
--- Arl_v3/Arl_v3/views.py
+++ Arl_v3/Arl_v3/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("Welcome in django website")
+    return render(request,'base.html')
